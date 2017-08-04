c2c48ba2e1775836156a7313cd07a7ed6005ccfd
Seb Martin <sebastien.martin@shopify.com>
2017-08-04 14:33:16 -0400

    remove duplicate refs from git-recent

--- gitz-recent.zsh
+++ gitz-recent.zsh
@@ -1,7 +1,7 @@
 # Show recent HEAD values by crubbing through reflog
 _gitz-recent() {
-	local list=$(git reflog --date=relative | grep 'checkout: moving from' | sed -E 's/^.*HEAD@{(.*)}.*moving from [^ ]* to ([^ ]*)/(\1)	\2/' | head -n 50)
-	local result=$(echo $list | fzf --delimiter='\t' --tabstop=2 --nth=2 --height=25% --reverse --header='Recent checkouts' | awk -F  "\t" '{print $2}')
+	local list=$(git log --date=relative -g '--pretty=format:%>(20)%gD%x09%gs' | grep 'checkout: moving from' | awk '{print (NR-1),$0}' | sed -E 's/checkout: moving from [^ ]* to ([^ ]*)/\1/' | head -n 1000 | sort -k 5 -k 1 | uniq -f 4 | sort -n | sed -e 's/^.*HEAD@{\(.*\)}/(\1)/' | column -t -s $'\t')
+	local result=$(echo $list | fzf --delimiter='\t' --tabstop=2 --nth=1 --height=25% --reverse --header='Recent checkouts' | sed -E 's/^[^)]*) *//')
 
 	gitz-append-text $result
 }
